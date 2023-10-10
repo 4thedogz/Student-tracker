e2ecb67886830698050a4c01beaa1edfb1f08df2
vedesh137 <65479153+vedesh137@users.noreply.github.com>
2023-10-10 22:49:35 +0000

    Duplicate staff username fix

--- App/views/user.py
+++ App/views/user.py
@@ -6,7 +6,8 @@ from.index import index_views
 
 from App.controllers import (
     create_user,
-    jwt_authenticate, 
+    jwt_authenticate,
+    get_user_by_username, 
     get_all_users,
     get_all_users_json,
     jwt_required
@@ -26,9 +27,17 @@ def get_users_action():
 
 @user_views.route('/api/users', methods=['POST'])
 def create_user_endpoint():
-    data = request.json
-    create_user(data['id'], data['username'], data['password'])
-    return jsonify({'message': f"user {data['username']} created"})
+  data = request.json
+  username = data.get('username')
+
+  # Check if a user with the same username already exists
+  existing_user = get_user_by_username(username)
+  if existing_user:
+    return jsonify(
+        {'message': f"User with username {username} already exists"}), 400
+
+  create_user(data['id'], data['username'], data['password'])
+  return jsonify({'message': f"User {data['username']} created"}), 201
 
 # @user_views.route('/users', methods=['POST'])
 # def create_user_action():

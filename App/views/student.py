c89e9483e9176c5640e5448cb11f12666fc8792a
vedesh137 <65479153+vedesh137@users.noreply.github.com>
2023-10-11 02:17:34 +0000

    separated views

--- App/views/student.py
+++ App/views/student.py
@@ -10,17 +10,10 @@ from App.controllers import (create_user, jwt_authenticate, get_all_users,
 
 student_views = Blueprint('student_views', __name__, template_folder='../templates')
 
-#--------------Student testing---------------------------------------------
-from App.controllers import ReviewController
+
 from App.controllers import StudentController
 
 student_controller = StudentController()
-review_controller = ReviewController()
-
-#karma
-from App.models.review import Reviews
-from App.controllers import create_karma_vote
-
 
 @student_views.route('/api/search', methods=['GET'])
 @jwt_required()
@@ -86,7 +79,7 @@ def add_student():
       }), 201
     else:
       return jsonify({"error": "Student creation failed"
-                      }), 500  # You can choose an appropriate status code
+                      }), 500 
 
   except Exception as e:
     # Handle any exceptions (e.g., database errors) here
@@ -108,90 +101,12 @@ def update_student(student_id):
       # If the student is updated successfully, return a success response
       return jsonify({"message": "Student updated successfully"}), 200
     else:
-      # If the student is not found or update fails, return an appropriate error response
+      # If the student is not found or update fails
       return jsonify({"error": "Student not found or update failed"
-                      }), 400  # You can choose an appropriate status code
+                      }), 400 
 
   except Exception as e:
     # Handle any exceptions (e.g., database errors) here
     print(str(e))
     return jsonify({"error": "Internal Server Error"}), 500
 
-
-@student_views.route("/api/reviews/<int:student_id>", methods=["POST"])
-@jwt_required()
-def log_review(student_id):
-  try:
-    # Get the logged-in student's ID from the JWT token
-    #student_id = get_jwt_identity()
-
-    # Get the JSON data from the request
-    data = request.get_json()
-
-    # Check if the required fields are present in the request
-    if "message" not in data:
-      return jsonify({"error": "Invalid request payload"}), 400
-
-    # Extract review data from the request
-    message = data["message"]
-    #staff_id = data["staff_id"]
-    #staff_id = 1  #todo: change this to use jwt identify
-    staff_id = jwt_current_user.id
-    # Call the create_log_review method from the ReviewController
-    new_review = review_controller.create_log_review(student_id, message,
-                                                     staff_id)
-
-    if new_review:
-      # If the review is created successfully, return a success response
-      return jsonify({
-          "message": "Review logged successfully",
-          "review_id": new_review.id
-      }), 201
-    else:
-      # If the review creation fails, return an appropriate error response
-      return jsonify({"error": "Review creation failed"
-                      }), 500  # You can choose an appropriate status code
-
-  except Exception as e:
-    # Handle any exceptions (e.g., database errors) here
-    print(str(e))
-    return jsonify({"error": "Internal Server Error"}), 500
-
-
-# ... (other routes)
-
-
-@student_views.route('/api/reviews/<int:review_id>/karma', methods=['POST'])
-@jwt_required()
-def karma_ranking(review_id):
-  try:
-    # Get the logged-in staff member's ID from the JWT token
-    staff_id = jwt_current_user.id
-    #staff_id = 2  #todo change this to jwt identify
-
-    # Get the JSON data from the request
-    data = request.get_json()
-
-    # Check if the required field 'value' is present in the request JSON
-    if 'value' not in data:
-      return jsonify({"error": "Invalid request payload"}), 400
-
-    # Extract the 'value' field from the request JSON
-    value = data['value']
-
-    # Ensure 'value' is either 1 (upvote) or -1 (downvote)
-    if value not in [1, -1]:
-      return jsonify({"error": "Invalid 'value' field"}), 400
-
-    # Call the create_karma_vote function to create or update a karma vote
-    new_vote = create_karma_vote(staff_id, review_id, value)
-
-    if new_vote:
-      return jsonify({"message": "Karma vote recorded successfully"}), 201
-    else:
-      return jsonify({"error": "Failed to record karma vote, staff already voted"
-                      }), 500  # You can choose an appropriate status code
-  except Exception as e:
-    # Handle any exceptions (e.g., database errors) here
-    print(str(e))
-    return jsonify({"error": "Internal Server Error"}), 500
